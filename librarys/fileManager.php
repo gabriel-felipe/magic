0b9a98e95418c75daab33954ec0dd97cbb0516a5
Gabriel <email@gabrielfelipe.com>
2013-09-26 21:32:11 -0300

    fixed security bug where folders of new scopes were generated at 0777

--- librarys/fileManager.php
+++ librarys/fileManager.php
@@ -17,13 +17,13 @@
             if(is_array($dirs)){
                 foreach($dirs as $dir=>$subdirs)
                     if(is_array($subdirs)){
-                        mkdir($base."/".$dir);
+                        mkdir($base."/".$dir,0775);
                         self::recursiveMkDir($subdirs,$base."/".$dir);
                     } else {
-                        mkdir($base."/".$subdirs);
+                        mkdir($base."/".$subdirs,0775);
                     }
             } else {
-                return mkdir($dirs);
+                return mkdir($dirs,0775);
             }
         }
         static function mkFiles($files,$base=__FILE__){
